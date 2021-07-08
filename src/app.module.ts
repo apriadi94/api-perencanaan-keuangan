0359ae4c17ae6eeb8b509c45db0282ae7ae2ee5b
Apriadi <dev.apriadi@gmail.com>
2021-07-08 19:32:34 +0700

    refactory app route

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { FinanceModule } from './finance/finance.module';
 
@@ -12,6 +11,5 @@ import { FinanceModule } from './finance/finance.module';
     FinanceModule
   ],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
