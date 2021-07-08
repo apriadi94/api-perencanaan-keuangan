84424c6502644f54cf5ceff786df82f8ec6efed8
Apriadi <dev.apriadi@gmail.com>
2021-07-08 16:16:49 +0700

    koneksi monggose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { FinanceModule } from './finance/finance.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/keuangan', {
+        autoCreate: true,
+      }),
+    FinanceModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
