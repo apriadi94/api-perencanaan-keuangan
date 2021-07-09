2514b927cc651d28b18cc0f10cc290f571f6332c
Apriadi <dev.apriadi@gmail.com>
2021-07-09 20:46:36 +0700

    menambahkan method untuk mengubah data

--- src/finance/finance.controller.ts
+++ src/finance/finance.controller.ts
@@ -8,11 +8,51 @@ export class FinanceController {
 
     @Get()
     async getAllData(){
-        return this.financeService.all()
+        const allDataFinance = await this.financeService.all()
+        return this.changeData(allDataFinance)
     }
 
     @Post()
     async addFinanceData(@Body() finance: Finance){
         return this.financeService.add(finance)
+    }
+
+    changeData(arrayData: any[]) {
+        return new Promise(resolve => {
+            const arrData: any[] = []
+            let newData: any = {}
+            let jumlah: any = {}
+
+
+            arrayData.forEach(item => {
+                jumlah[item.tanggal] ? jumlah[item.tanggal]++ : jumlah[item.tanggal] = 1
+                newData[item.tanggal] = arrayData.filter(itemFilter => itemFilter.tanggal === item.tanggal)
+            })
+
+            const objectArray: any = Object.entries(newData);
+
+            objectArray.forEach(([key, value]) => {
+                let pemasukan: number = 0
+                let pengeluaran: number = 0
+
+                value.forEach(item => {
+                    if(item.jenis === 'pemasukan'){
+                        pemasukan += item.jumlah
+                    }else{
+                        pengeluaran += item.jumlah
+                    }
+                })
+
+                arrData.push({
+                    tanggal: key,
+                    data: value,
+                    pemasukan,
+                    pengeluaran
+                })
+            });
+            
+
+            resolve(arrData)
+        })
     }
 }
