1914226751f44a273b449b948d23e6a020274cad
Apriadi <dev.apriadi@gmail.com>
2021-07-10 18:51:15 +0700

    memindah change data ke service

--- src/finance/finance.service.ts
+++ src/finance/finance.service.ts
@@ -10,7 +10,84 @@ export class FinanceService {
     ){}
 
     async all(){
-        return this.FinanceModel.find().exec()
+        const allData = await this.FinanceModel.find().exec()
+        return this.changeData(allData)
+    }
+
+    changeData(arrayData) {
+        return new Promise(resolve => {
+
+            interface ArrData {
+                tanggal: string;
+                data: Array<Data>;
+                pemasukan: number,
+                pengeluaran: number
+            }
+
+            interface Data {
+                _id: string;
+                tanggal: string;
+                jumlah: number;
+                idKategori: number;
+                keterangan: string;
+                jenis: string;
+                __v: number;
+            }
+
+            interface NewData {
+                [key: string]: Array<Data>
+            }
+
+            interface Tanggal {
+                [key: string]: number
+            }
+            
+            const arrData: Array<ArrData> = []
+
+            let newData: NewData = {}
+            let jumlah: Tanggal = {}
+
+
+            arrayData.forEach(item => {
+                jumlah[item.tanggal] ? jumlah[item.tanggal]++ : jumlah[item.tanggal] = 1
+                newData[item.tanggal] = arrayData.filter(itemFilter => itemFilter.tanggal === item.tanggal)
+            })
+
+            const objectArray: Array<Array<string | Array<Data>>> = Object.entries(newData);
+
+
+            objectArray.forEach(([key, value]) => {
+                let pemasukan = 0
+                let pengeluaran = 0
+                let tanggal: string
+                let data: Array<Data>
+
+                if(typeof key === 'string'){
+                    tanggal = key
+                }
+
+                if (Array.isArray(value)) {
+                    value.forEach(item => {
+                        if(item.jenis === 'pemasukan'){
+                            pemasukan += item.jumlah
+                        }else{
+                            pengeluaran += item.jumlah
+                        }
+                    })
+
+                    data = value
+                }
+
+                arrData.push({
+                    tanggal,
+                    data,
+                    pemasukan,
+                    pengeluaran
+                })
+            });
+            
+            resolve(arrData)
+        })
     }
 
     async add(finance: Finance): Promise<Finance> {
