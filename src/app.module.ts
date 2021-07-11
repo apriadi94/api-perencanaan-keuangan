b0206dd994fd5d7375b29e581f22e21f58de05c3
Apriadi <dev.apriadi@gmail.com>
2021-07-11 16:23:48 +0700

    menambahkan category route

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { FinanceModule } from './finance/finance.module';
+import { CategoryModule } from './category/category.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/keuangan', {
         autoCreate: true,
       }),
-    FinanceModule
+    FinanceModule,
+    CategoryModule
   ],
   controllers: [AppController],
 })
