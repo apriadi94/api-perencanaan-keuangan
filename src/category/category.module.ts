18ac2b5740ccfc6240f8a53e828b913e5c298e55
Apriadi <dev.apriadi@gmail.com>
2021-07-11 18:12:39 +0700

    integrasi model category

--- src/category/category.module.ts
+++ src/category/category.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { CategoryController } from './category.controller';
 import { CategoryService } from './category.service';
+import { MongooseModule } from '@nestjs/mongoose'
+import { Category, CategorySchema } from './category.model'
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Category.name, schema: CategorySchema }])
+  ],
   controllers: [CategoryController],
   providers: [CategoryService]
 })
--- src/category/category.service.ts
+++ src/category/category.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose'
+import { Category, CategoryDocument } from './category.model'
 
 @Injectable()
-export class CategoryService {}
+export class CategoryService {
+    constructor(
+        @InjectModel(Category.name) private readonly CategoryModel: Model<CategoryDocument>
+    ) {}
+
+    async allCategory(){
+        return this.CategoryModel.find().exec()
+    }
+}
